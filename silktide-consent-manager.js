92788e56c1b23b1e57e5b57616c05f76bdfeb8e1
rawf2 <153083956+rawf2@users.noreply.github.com>
2025-05-13 13:48:53 +0200

    Update silktide-consent-manager.js

--- silktide-consent-manager.js
+++ silktide-consent-manager.js
@@ -745,9 +745,9 @@ class SilktideCookieBanner {
     }
 
     // Check Cookie Icon exists before trying to add event listeners
-    if (this.cookieIcon) {
+    //if (this.cookieIcon) {
 
-      this.cookieIcon.addEventListener('click', () => {
+      /*this.cookieIcon.addEventListener('click', () => {
         // If modal is not found, create it
         if (!this.modal) {
           this.createModal();
@@ -766,7 +766,7 @@ class SilktideCookieBanner {
       });
     }
   }
-
+*/
   getBannerSuffix() {
     if (this.config.bannerSuffix) {
       return '_' + this.config.bannerSuffix;
